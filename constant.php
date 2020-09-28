f6dc324b3e707511a9c8e4d9161ca408523fa410
Diki Fauzi <akbareskhart@gmail.com>
2020-09-28 12:44:08 +0700

    Menambahkan Constant

---
+++ constant.php
@@ -0,0 +1,41 @@
+<?php 
+
+// define('NAMA', 'Diki Fauzi');
+
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 21;
+// echo UMUR;
+
+
+
+
+// class Coba {
+// 	const NAMA = 'Diki Fauzi';
+// }
+
+// echo Coba::NAMA;
+
+
+// echo __FILE__;
+
+
+// function coba() {
+// 	return __FUNCTION__;
+// }
+
+// echo coba();
+
+
+
+class Coba {
+	public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+
+
+ ?>
